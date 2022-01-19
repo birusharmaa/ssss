586e3f421d1f21574f82a47b8d13c7364a63bce3
mrrawatofficial <rahulrawat520@gmail.com>
2022-01-19 13:40:28 +0530

    designed login page
    designed login page
    added csslinks page containing all css links and jslinks page containing all js links

---
+++ app/Views/admin/layout/cssLinks.php
@@ -0,0 +1,6 @@
+<link rel="shortcut icon" href="<?=base_url();?>/assets/images/favicon.png" />
+<link rel="stylesheet" href="<?=base_url();?>/assets/vendors/iconfonts/font-awesome/css/all.min.css">
+<link rel="stylesheet" href="<?=base_url();?>/assets/vendors/css/vendor.bundle.base.css">
+<link rel="stylesheet" href="<?=base_url();?>/assets/vendors/css/vendor.bundle.addons.css">
+<link rel="stylesheet" href="<?=base_url();?>/assets/css/style.css">
+<link rel="stylesheet" href="<?=base_url();?>/assets/css/custom.min.css">
--- app/Views/admin/login.php
+++ app/Views/admin/login.php
@@ -4,13 +4,8 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>XLAcademy Admin</title>
-  <link rel="stylesheet" href="<?=base_url();?>/assets/vendors/iconfonts/font-awesome/css/all.min.css">
-  <link rel="stylesheet" href="<?=base_url();?>/assets/vendors/css/vendor.bundle.base.css">
-  <link rel="stylesheet" href="<?=base_url();?>/assets/vendors/css/vendor.bundle.addons.css">
-  <link rel="stylesheet" href="<?=base_url();?>/assets/css/style.css">
-  <link rel="shortcut icon" href="<?=base_url();?>/assets/images/favicon.png" />
+  <?php include 'layout/cssLinks.php'; ?>
 </head>
-
 <body>
   <div class="container-scroller">
     <div class="container-fluid page-body-wrapper full-page-wrapper">
@@ -19,19 +14,19 @@
           <div class="col-lg-4 mx-auto">
             <div class="auth-form-light text-left p-5">
               <div class="brand-logo">
-                <img src="<?=base_url();?>/assets/images/logo.svg" alt="logo">
+                <img src="<?=base_url();?>/assets/images/logo.png" alt="logo">
               </div>
               <h4>Hello! let's get started</h4>
               <h6 class="font-weight-light">Sign in to continue.</h6>
-              <form class="pt-3">
+              <form class="pt-3" id="loginForm">
                 <div class="form-group">
-                  <input type="email" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Username">
+                  <input type="email" class="form-control form-control-lg" id="email" placeholder="Email" name="email">
                 </div>
                 <div class="form-group">
-                  <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password">
+                  <input type="password" class="form-control form-control-lg" id="password" placeholder="Password" name="password">
                 </div>
                 <div class="mt-3">
-                  <a class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" href="<?=base_url();?>/assets/index-2.html">SIGN IN</a>
+                  <button class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" type="submit" >Sign In</button>
                 </div>
                 <div class="my-2 d-flex justify-content-between align-items-center">
                   <div class="form-check">
@@ -42,14 +37,6 @@
                   </div>
                   <a href="#" class="auth-link text-black">Forgot password?</a>
                 </div>
-                <div class="mb-2">
-                  <button type="button" class="btn btn-block btn-facebook auth-form-btn">
-                    <i class="fab fa-facebook-f mr-2"></i>Connect using facebook
-                  </button>
-                </div>
-                <div class="text-center mt-4 font-weight-light">
-                  Don't have an account? <a href="register.html" class="text-primary">Create</a>
-                </div>
               </form>
             </div>
           </div>
@@ -57,12 +44,28 @@
       </div>
     </div>
   </div>
-  <script src="<?=base_url();?>/assets/vendors/js/vendor.bundle.base.js"></script>
-  <script src="<?=base_url();?>/assets/vendors/js/vendor.bundle.addons.js"></script>
-  <script src="<?=base_url();?>/assets/js/off-canvas.js"></script>
-  <script src="<?=base_url();?>/assets/js/hoverable-collapse.js"></script>
-  <script src="<?=base_url();?>/assets/js/misc.js"></script>
-  <script src="<?=base_url();?>/assets/js/settings.js"></script>
-  <script src="<?=base_url();?>/assets/js/todolist.js"></script>
+  <?php include 'layout/jsLinks.php'; ?>
+  <script>
+    $("#loginForm").validate({
+      errorClass: "is-invalid",
+      rules: {
+        password: {
+          required: true,
+          minlength: 5
+        },
+        email: {
+          required: true,
+          email: true
+        },
+      },
+      messages: {
+        password: {
+          required: "Please provide a password",
+          minlength: "Your password must be at least 5 characters long"
+        },
+        email: "Please enter a valid email address",
+      },      
+    });
+  </script>
 </body>
 </html>
