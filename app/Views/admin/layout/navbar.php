c3448d644baa71742edfbdd181f0fda16c840d09
mrrawatofficial <rahulrawat520@gmail.com>
2022-01-20 10:43:42 +0530

    logo

--- app/Views/admin/layout/navbar.php
+++ app/Views/admin/layout/navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row default-layout-navbar">
       <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
         <a class="navbar-brand brand-logo" href="<?=base_url();?>/dashboard"><img src="<?=base_url();?>/assets/images/logo.png" alt="logo"/></a>
-        <a class="navbar-brand brand-logo-mini" href="<?=base_url();?>/dashboard"><img src="<?=base_url();?>/assets/images/logo.png" alt="logo"/></a>
+        <a class="navbar-brand brand-logo-mini" href="<?=base_url();?>/dashboard"><img src="<?=base_url();?>/assets/images/logo-sm.png" alt="logo"/></a>
       </div>
       <div class="navbar-menu-wrapper d-flex align-items-stretch">
         <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
