132c873f96da4d8d8a077d5b702b4a7173380f5e
birusharma <biru.sartia@gmail.com>
2022-03-08 12:25:31 +0530

    Loader message chagnes

--- app/Views/admin/layout/loader.php
+++ app/Views/admin/layout/loader.php
@@ -1,5 +1,6 @@
 <div class="loader">
         <div class="loader-circle">
-                
+                <img src="<?= base_url(); ?>/assets/images/arrow.gif" alt="logo" width="80" /> <br/>
+                <span class="text-white">Please wait...</span>
         </div>
 </div>
